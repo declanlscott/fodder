5a422a3a572093b6324e3d63a587ccd51e7d7d2f
dscott1008 <dscott1008@gmail.com>
2023-09-20 00:52:38 -0500

    test: :white_check_mark: Get mock response from s3 for test

--- backend/lambdas/restaurant/main_test.go
+++ backend/lambdas/restaurant/main_test.go
@@ -3,13 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
 	"net/http"
 	"os"
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 type MockHttpClient struct {
@@ -21,15 +25,67 @@ func (mockHttpClient *MockHttpClient) Do(req *http.Request) (*http.Response, err
 	return mockHttpClient.Response, mockHttpClient.Error
 }
 
-func TestScrapeRestaurantWithMockResponse(t *testing.T) {
-	file, err := os.Open("mock_response.html")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	defer file.Close()
+func getMockResponse(fileName string) ([]byte, error) {
+	var file *os.File
 
-	contents, err := io.ReadAll(file)
+	_, err := os.Stat(fileName)
+	if err == nil {
+		// File exists
+		file, err = os.Open(fileName)
+		if err != nil {
+			// An error occurred while opening the file
+			return nil, err
+		}
+		defer file.Close()
+
+		return io.ReadAll(file)
+	} else if os.IsNotExist(err) {
+		// File does not exist
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			return nil, err
+		}
+
+		s3Client := s3.NewFromConfig(cfg)
+
+		result, err := s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
+			Bucket: aws.String("fodder-test-files"),
+			Key:    aws.String(fmt.Sprintf("mock-responses/%s", fileName)),
+		})
+		if err != nil {
+			// An error occurred while getting the file from S3
+			return nil, err
+		}
+		defer result.Body.Close()
+
+		file, err = os.Create(fileName)
+		if err != nil {
+			// An error occurred while creating the file
+			return nil, err
+		}
+		defer file.Close()
+
+		body, err := io.ReadAll(result.Body)
+		if err != nil {
+			// An error occurred while reading the file from S3
+			return nil, err
+		}
+
+		_, err = file.Write(body)
+		if err != nil {
+			// An error occurred while writing the file to disk
+			return nil, err
+		}
+
+		return body, nil
+	} else {
+		// An error occurred while checking if the file exists
+		return nil, err
+	}
+}
+
+func TestScrapeRestaurant(t *testing.T) {
+	contents, err := getMockResponse("restaurant.mock.html")
 	if err != nil {
 		t.Error(err)
 		return
--- terraform/main.tf
+++ terraform/main.tf
@@ -164,3 +164,29 @@ resource "aws_lambda_permission" "allow_restaurant_api" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.fodder.execution_arn}/*/GET/restaurant/*"
 }
+
+# Bucket for test files
+resource "aws_s3_bucket" "fodder_test_files" {
+  bucket        = "fodder-test-files"
+  force_destroy = true
+}
+resource "aws_s3_bucket_versioning" "fodder_test_files_bucket_versioning" {
+  bucket = aws_s3_bucket.fodder_test_files.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "fodder_test_files_crypto_config" {
+  bucket = aws_s3_bucket.fodder_test_files.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+resource "aws_s3_object" "mock_responses" {
+  bucket       = aws_s3_bucket.fodder_test_files.bucket
+  key          = "mock-responses/"
+  content_type = "application/x-directory"
+  acl          = "private"
+}
