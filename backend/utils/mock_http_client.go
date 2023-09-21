84d10a74022b02582df8b76e2cd4bc1ab071f972
dscott1008 <dscott1008@gmail.com>
2023-09-20 21:48:46 -0500

    feat: :clown_face: mock http client

---
+++ backend/utils/mock_http_client.go
@@ -0,0 +1,12 @@
+package utils
+
+import "net/http"
+
+type MockHttpClient struct {
+	Response *http.Response
+	Error    error
+}
+
+func (mockHttpClient *MockHttpClient) Do(req *http.Request) (*http.Response, error) {
+	return mockHttpClient.Response, mockHttpClient.Error
+}
