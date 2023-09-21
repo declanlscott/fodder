247b70232495b1ef4a781893d15d8f2fec1d7e45
dscott1008 <dscott1008@gmail.com>
2023-09-20 22:31:36 -0500

    refactor: :recycle: Use utils package for tests

--- backend/lambdas/flavor/main_test.go
+++ backend/lambdas/flavor/main_test.go
@@ -7,92 +7,21 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"reflect"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/dscott1008/fodder/backend/utils"
 )
 
-type MockHttpClient struct {
-	Response *http.Response
-	Error    error
-}
-
-func (mockHttpClient *MockHttpClient) Do(req *http.Request) (*http.Response, error) {
-	return mockHttpClient.Response, mockHttpClient.Error
-}
-
-func getMockResponse(fileName string) ([]byte, error) {
-	var file *os.File
-
-	_, err := os.Stat(fileName)
-	if err == nil {
-		// File exists
-		file, err = os.Open(fileName)
-		if err != nil {
-			// An error occurred while opening the file
-			return nil, err
-		}
-		defer file.Close()
-
-		return io.ReadAll(file)
-	} else if os.IsNotExist(err) {
-		// File does not exist
-		cfg, err := config.LoadDefaultConfig(context.TODO())
-		if err != nil {
-			return nil, err
-		}
-
-		s3Client := s3.NewFromConfig(cfg)
-
-		result, err := s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
-			Bucket: aws.String("fodder-test-files"),
-			Key:    aws.String(fmt.Sprintf("mock-responses/%s", fileName)),
-		})
-		if err != nil {
-			// An error occurred while getting the file from S3
-			return nil, err
-		}
-		defer result.Body.Close()
-
-		file, err = os.Create(fileName)
-		if err != nil {
-			// An error occurred while creating the file
-			return nil, err
-		}
-		defer file.Close()
-
-		body, err := io.ReadAll(result.Body)
-		if err != nil {
-			// An error occurred while reading the file from S3
-			return nil, err
-		}
-
-		_, err = file.Write(body)
-		if err != nil {
-			// An error occurred while writing the file to disk
-			return nil, err
-		}
-
-		return body, nil
-	} else {
-		// An error occurred while checking if the file exists
-		return nil, err
-	}
-}
-
 func TestScrapeFlavor(t *testing.T) {
-	contents, err := getMockResponse("flavor.mock.html")
+	contents, err := utils.GetMockResponse("flavor.mock.html")
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	mockClient := &MockHttpClient{
+	mockClient := &utils.MockHttpClient{
 		Response: &http.Response{
 			StatusCode: 200,
 			Body:       io.NopCloser(bytes.NewReader(contents)),
--- backend/lambdas/restaurant/go.mod
+++ backend/lambdas/restaurant/go.mod
@@ -5,15 +5,15 @@ go 1.21.1
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/aws/aws-lambda-go v1.41.0
-	github.com/aws/aws-sdk-go-v2/config v1.18.40
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.38.5
+	github.com/dscott1008/fodder/backend/utils v0.0.0-20230921024846-84d10a74022b
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.21.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.13 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.38 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.41 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.39 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.11 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.41 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.35 // indirect
@@ -23,8 +23,9 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.36 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.35 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.15.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.39.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.14.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.16.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.17.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.22.0 // indirect
 	github.com/aws/smithy-go v1.14.2 // indirect
 	golang.org/x/net v0.7.0 // indirect
