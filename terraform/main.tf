39fadb335460d6d297c610e95c82d70261a8d957
dscott1008 <dscott1008@gmail.com>
2023-09-30 21:45:21 -0500

    refactor: ♻️ Refactor route keys
    Consistent resource names, `restaurants` and `flavors`

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,7 +117,7 @@ resource "aws_apigatewayv2_integration" "restaurant" {
 }
 resource "aws_apigatewayv2_route" "restaurant" {
   api_id    = aws_apigatewayv2_api.fodder.id
-  route_key = "GET /restaurant/{slug}"
+  route_key = "GET /restaurants/{slug}"
   target    = "integrations/${aws_apigatewayv2_integration.restaurant.id}"
 }
 resource "aws_apigatewayv2_integration" "flavor" {
@@ -130,7 +130,7 @@ resource "aws_apigatewayv2_integration" "flavor" {
 }
 resource "aws_apigatewayv2_route" "flavor" {
   api_id    = aws_apigatewayv2_api.fodder.id
-  route_key = "GET /flavor/{slug}"
+  route_key = "GET /flavors/{slug}"
   target    = "integrations/${aws_apigatewayv2_integration.flavor.id}"
 }
 resource "aws_apigatewayv2_integration" "flavors" {
@@ -259,7 +259,7 @@ resource "aws_lambda_permission" "allow_restaurant_api" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.restaurant.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.fodder.execution_arn}/*/GET/restaurant/*"
+  source_arn    = "${aws_apigatewayv2_api.fodder.execution_arn}/*/GET/restaurants/*"
 }
 
 variable "flavor_function_name" {
@@ -299,7 +299,7 @@ resource "aws_lambda_permission" "allow_flavor_api" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.flavor.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.fodder.execution_arn}/*/GET/flavor/*"
+  source_arn    = "${aws_apigatewayv2_api.fodder.execution_arn}/*/GET/flavors/*"
 }
 
 variable "flavors_function_name" {
