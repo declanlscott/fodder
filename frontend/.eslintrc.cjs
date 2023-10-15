0d4f1e2c901778fba073c05b9de615be513b9e02
Declan <dscott1008@gmail.com>
2023-10-14 21:10:49 -0500

    chore: 🔧 Fix eslint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:@tanstack/eslint-plugin-query",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
