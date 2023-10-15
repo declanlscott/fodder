9e01a3dfd57e8967dc6df18117f55789cbe43536
Declan <dscott1008@gmail.com>
2023-10-14 21:11:27 -0500

    style: 🎨 `format` script

--- frontend/src/pages/Locate.tsx
+++ frontend/src/pages/Locate.tsx
@@ -14,34 +14,34 @@ export function LocatePage() {
   const isEmpty = (data?.length ?? -1) === 0;
 
   return (
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <div className="col-span-1">
-          <LocateCard />
-        </div>
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="col-span-1">
+        <LocateCard />
+      </div>
 
-        <div className="col-span-1 lg:col-span-2 xl:col-span-3">
-          <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
-            {isLoading ? (
-              Array.from({ length: 3 }).map((_, index) => (
-                <FodCardSkeleton key={index} />
-              ))
-            ) : isEmpty ? (
+      <div className="col-span-1 lg:col-span-2 xl:col-span-3">
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
+          {isLoading ? (
+            Array.from({ length: 3 }).map((_, index) => (
+              <FodCardSkeleton key={index} />
+            ))
+          ) : isEmpty ? (
+            <Card className="col-span-full flex h-64 flex-col items-center justify-center gap-4 text-muted-foreground">
+              <DroppedCone className="h-28 fill-muted-foreground" />
+              {"No locations found..."}
+            </Card>
+          ) : (
+            data?.map((restaurant) => (
+              <FodCard key={restaurant.slug} restaurant={restaurant} />
+            )) ?? (
               <Card className="col-span-full flex h-64 flex-col items-center justify-center gap-4 text-muted-foreground">
-                <DroppedCone className="h-28 fill-muted-foreground" />
-                {"No locations found..."}
+                <Dessert className="h-28 w-28 text-muted-foreground" />
+                {"Nothing here yet..."}
               </Card>
-            ) : (
-              data?.map((restaurant) => (
-                <FodCard key={restaurant.slug} restaurant={restaurant} />
-              )) ?? (
-                <Card className="col-span-full flex h-64 flex-col items-center justify-center gap-4 text-muted-foreground">
-                  <Dessert className="h-28 w-28 text-muted-foreground" />
-                  {"Nothing to see here..."}
-                </Card>
-              )
-            )}
-          </div>
+            )
+          )}
         </div>
       </div>
+    </div>
   );
 }
