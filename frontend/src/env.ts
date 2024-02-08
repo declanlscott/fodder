800217b13fe30972fe899bdadc70cdb881e64c08
Declan L. Scott <dscott1008@gmail.com>
2024-02-08 10:33:14 -0600

    refactor(frontend): Use `valibot` for env validation
    instead of zod

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,17 +1,11 @@
-import { createEnv } from "@t3-oss/env-core";
-import { z, ZodError } from "zod";
+import { string, url } from "valibot";
+
+import { createEnv } from "~/lib/vendor/env";
 
 export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
-    VITE_API_BASE_URL: z.string().url(),
+    VITE_API_BASE_URL: string([url()]),
   },
   runtimeEnv: import.meta.env,
-  onValidationError: (error: ZodError) => {
-    console.error(
-      "❌ Invalid environment variables:",
-      error.flatten().fieldErrors,
-    );
-    throw new Error("Invalid environment variables");
-  },
 });
