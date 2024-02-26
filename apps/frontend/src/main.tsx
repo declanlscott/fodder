d51e81f94b909808c1ce44a7d0d4b04918cbe9ef
Declan L. Scott <dscott1008@gmail.com>
2024-02-26 17:03:40 -0600

    feat(frontend): font optimization

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "@repo/ui/globals.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
+import { FontaineTransform } from "fontaine";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    FontaineTransform.vite({
+      fallbacks: ["Arial", "Roboto"],
+    }),
+  ],
 });
