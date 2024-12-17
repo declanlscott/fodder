5957d4025ab91331b3f48f848d9f243f01b42ed9
Declan L. Scott <declanlscott@gmail.com>
2024-12-16 22:54:25 -0600

    refactor: `ref` as a prop on non radix components

--- packages/ui/src/components/skeleton.tsx
+++ packages/ui/src/components/skeleton.tsx
@@ -1,19 +1,14 @@
 import { cn } from "../utils";
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn(
-        "animate-pulse rounded-md bg-muted",
-        "relative isolate overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-muted-foreground/30 before:to-transparent",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+export type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
 
-export { Skeleton };
+export const Skeleton = ({ className, ...props }: SkeletonProps) => (
+  <div
+    className={cn(
+      "animate-pulse rounded-md bg-muted",
+      "relative isolate overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-muted-foreground/30 before:to-transparent",
+      className,
+    )}
+    {...props}
+  />
+);
